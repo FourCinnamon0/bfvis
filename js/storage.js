7224dc9791aad9457d33fc5493b4eda4d2ae8762
pitust <stelmaszek.piotrpilot@gmail.com>
2020-05-01 08:51:17 +0100

    Re-Formatted to the same style
    4 spaces & if (<cond>) { <--- in the same line

--- js/storage.js
+++ js/storage.js
@@ -1,48 +1,48 @@
 var modal = document.getElementById("modal");
 var uns = document.getElementById('uns');
-function updatebitshow(){
+function updatebitshow() {
     var bitshow = document.getElementById("bitshow");
     var bitsin = document.getElementById('bits');
     bitshow.innerHTML = bitsin.value;
     bits = bitsin.value
     localStorage.setItem("bits", bitsin.value);
-    if(bits == bitsin.value){
-      uns.classList.add('hidden')
-    }else{
-      uns.classList.remove('hidden')
+    if (bits == bitsin.value) {
+        uns.classList.add('hidden')
+    } else {
+        uns.classList.remove('hidden')
     }
-  }
-  var bitsin = document.getElementById('bits');
-  bitsin.value = localStorage.getItem("bits");
-  var bits = localStorage.getItem("bits");
-  updatebitshow();
-  uns.classList.add('hidden')
-  function updatedelshow(){
+}
+var bitsin = document.getElementById('bits');
+bitsin.value = localStorage.getItem("bits");
+var bits = localStorage.getItem("bits");
+updatebitshow();
+uns.classList.add('hidden')
+function updatedelshow() {
     var delshow = document.getElementById("delshow");
     var delay = document.getElementById('delay');
     delshow.innerHTML = delay.value;
     //localStorage.setItem("del", delay.value);
-  }
-  /* var delay = document.getElementById('delay');
-  delay.value = localStorage.getItem("del"); */
-  updatedelshow();
+}
+/* var delay = document.getElementById('delay');
+delay.value = localStorage.getItem("del"); */
+updatedelshow();
 
-  document.getElementById('input-file').addEventListener('change', getFile)
+document.getElementById('input-file').addEventListener('change', getFile)
 
-  async function getFile(event) {
+async function getFile(event) {
     const input = event.target
-    if('files' in input && input.files.length > 0) {
+    if ('files' in input && input.files.length > 0) {
         let fileData = await input.files.item(0).arrayBuffer().then(e => {
             let dec = new TextDecoder()
             return dec.decode(e);
         });
         var str = fileData;
         var arr = str.match(/\[ Designed to run on a ([0-9]+) bit interpreter ]/);
-        if(arr) {
-          let [,b] = arr
-              bits = +b;
-              document.getElementById('bits').value = bits;
-              updatebitshow();
+        if (arr) {
+            let [, b] = arr
+            bits = +b;
+            document.getElementById('bits').value = bits;
+            updatebitshow();
             document.getElementById('source').value = fileData.split('\n').slice(1).join('\n');
         } else {
             document.getElementById('source').value = fileData;
@@ -55,26 +55,26 @@ function updatebitshow(){
 
 // Function to download data to a file
 function download(data, filename) {
-  var file = new Blob([data], {type:'text/plain'});
-  if (window.navigator.msSaveOrOpenBlob) // IE10+
-      window.navigator.msSaveOrOpenBlob(file, filename);
-  else { // Others
-      var a = document.createElement("a"),
-              url = URL.createObjectURL(file);
-      a.href = url;
-      a.download = filename;
-      document.body.appendChild(a);
-      a.click();
-      setTimeout(function() {
-          document.body.removeChild(a);
-          window.URL.revokeObjectURL(url);  
-      }, 0); 
-  }
+    var file = new Blob([data], { type: 'text/plain' });
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
 }
-function downloadbf(){
-  var data = "[ Designed to run on a " + bits +" bit interpreter ]\n" + document.getElementById('source').value
-  var filename = document.getElementById('dfinput').value
-  download(data, filename)
+function downloadbf() {
+    var data = "[ Designed to run on a " + bits + " bit interpreter ]\n" + document.getElementById('source').value
+    var filename = document.getElementById('dfinput').value
+    download(data, filename)
 }
 
 
@@ -85,18 +85,18 @@ var ot = document.getElementById("ot");
 var closem = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-ot.onclick = function() {
-  modal.style.display = "block";
+ot.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-closem.onclick = function() {
-  modal.style.display = "none";
+closem.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
